489698ab88420a51e1b4681c9fb195a9b7667f8e
Maxb <bittmanmax@gmail.com>
2023-10-13 11:07:25 +0200

    Switching back from stage environment to production

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,7 @@ export const isDevelopment = env.startsWith("development");
 export const config = {
   // This is invisv's test deployment value
   vpaas: "vpaas-magic-cookie-4c6a7be7b42f4dbab6b3792a9b38fb6c",
-  webrtc_domain: "stage.8x8.vc",
+  webrtc_domain: "8x8.vc",
 };
 
 export const isProduction = env === "production";
