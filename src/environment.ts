2315c97acc25cca9308ada9ec582be0f21e74ff7
Maxb <bittmanmax@gmail.com>
2023-10-13 10:55:59 +0200

    Use non-pilot jaas magic-cookie value

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ export const env = process.env.ENVIRONMENT ?? "local";
 export const isDevelopment = env.startsWith("development");
 export const config = {
   // This is invisv's test deployment value
-  vpaas: "vpaas-magic-cookie-ffb7afc059534f0497e2ee68d9947e02",
+  vpaas: "vpaas-magic-cookie-4c6a7be7b42f4dbab6b3792a9b38fb6c",
   webrtc_domain: "stage.8x8.vc",
 };
 
